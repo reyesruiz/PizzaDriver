72237c403898ae7a4835b871ff78e9b6078f23fd
Reyes Ruiz <ruiz.reyes@yahoo.com>
2021-06-18 14:39:48 -0700

    Adding clear data
    Need to implement a better way to use navigation menu.
    Need to implement a message if sure if want to delete data.

--- app/src/main/java/com/digitalruiz/pizzadriver/settings.java
+++ app/src/main/java/com/digitalruiz/pizzadriver/settings.java
@@ -1,16 +1,24 @@
 package com.digitalruiz.pizzadriver;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.navigation.NavigationView;
 
 
 public class settings extends AppCompatActivity {
     SQLiteDBHelper pizzaDriverDB;
-    Button clear_button;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +27,27 @@ public class settings extends AppCompatActivity {
 
         pizzaDriverDB = new SQLiteDBHelper(this);
 
-        clear_button = findViewById(R.id.clear_data_btn);
-        clear_button.setOnClickListener(new View.OnClickListener() {
+        NavigationView nav_draw_settings = (NavigationView) findViewById(R.id.settings_navigationView);
+        MenuItem clear_data = nav_draw_settings.getMenu().findItem(R.id.clear_data_menu).setVisible(true);
+
+        clear_data.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
             @Override
-            public void onClick(View v) {
+            public boolean onMenuItemClick(MenuItem item) {
                 pizzaDriverDB.deleteData();
+                return false;
             }
         });
+
+
+
+
+        //clear_button = findViewById(R.id.clear_data_menu);
+        //clear_data.setOnClickListener(new View.OnClickListener() {
+        //    @Override
+        //    public void onClick(View v) {
+        //        pizzaDriverDB.deleteData();
+        //    }
+        //});
     }
 
 
