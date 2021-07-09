c5780e4d0286ca043c64dd25a7d3b00c39e4e974
Reyes Ruiz <reyes@digitalruiz.com>
2021-07-09 15:18:40 -0700

    Unit test 1 (#21)
    * First unit test

---
+++ app/src/test/java/com/digitalruiz/pizzadriver/ValidatorOrderText.kt
@@ -0,0 +1,25 @@
+package com.digitalruiz.pizzadriver
+
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class ValidatorOrderText  {
+
+    @Test
+    fun whenOrderNumberIsValid(){
+        val orderNumber = 100
+        val result = Validator.validateOrderNumber(orderNumber)
+        assertThat(result).isEqualTo(true)
+    }
+
+    @Test
+    fun whenOrderNumberIsInvalid(){
+        val orderNumber = -1
+        val result = Validator.validateOrderNumber(orderNumber)
+        assertThat(result).isEqualTo(false)
+    }
+}
