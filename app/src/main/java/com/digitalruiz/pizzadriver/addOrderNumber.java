999d1a21b80c9af5063466145b5d44206768edc2
Reyes Ruiz <reyes_ruiz@digitalruiz.com>
2021-11-08 19:41:19 -0800

    Removing not needed code from addOrderNumber.java

--- app/src/main/java/com/digitalruiz/pizzadriver/addOrderNumber.java
+++ app/src/main/java/com/digitalruiz/pizzadriver/addOrderNumber.java
@@ -18,39 +18,19 @@ public class addOrderNumber extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_order_number);
 
-
-
-        Intent intent = getIntent();
         final InputMethodManager imm = (InputMethodManager) this.getSystemService(Context.INPUT_METHOD_SERVICE);
         final EditText orderNumberText = this.findViewById(R.id.orderNumber);
-
-        final Integer oldOrderNumber;
-
 
         assert imm != null;
         imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
 
-        if (intent.hasExtra("orderNumber")) {
-            final int orderNumber = Objects.requireNonNull(intent.getExtras()).getInt("orderNumber");
-            orderNumberText.setText(Integer.toString(orderNumber));
-            oldOrderNumber = orderNumber;
-        }
-        else {
-            oldOrderNumber = null;
-        }
         orderNumberText.requestFocus();
-
 
         orderNumberText.setOnEditorActionListener((v, actionId, event) -> {
             if(actionId == EditorInfo.IME_ACTION_DONE){
                 int orderNumber =  Integer.parseInt(orderNumberText.getText().toString());
                 Intent addOrderIntent = new Intent(addOrderNumber.this, AddOrder.class);
                 addOrderIntent.putExtra("orderNumber", orderNumber);
-                if (oldOrderNumber != null) {
-                    if (orderNumber != oldOrderNumber) {
-                        addOrderIntent.putExtra("oldOrderNumber", oldOrderNumber);
-                    }
-                }
                 startActivity(addOrderIntent);
             }
             return false;
